25cbf12e21494c336fd3625e8a6a5986fa903152
kim-limit <gimjaehan@gimjaehan-ui-MacBookPro.local>
2023-07-04 11:43:48 +0900

    feat: command 및 error 메세지 상수화 Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- constant.js
+++ constant.js
@@ -1,7 +1,35 @@
-const STATUS = Object.freeze({
+const freeze = (object) => {
+  return Object.freeze(object);
+};
+
+const STATUS = freeze({
   TODO: "todo",
   DOING: "doing",
   DONE: "done",
 });
 
-module.exports = STATUS;
+const COMMAND = freeze({
+  SHOW: "show",
+  ADD: "add",
+  DELETE: "delete",
+  UPDATE: "update",
+});
+
+const SUB_COMMAND = freeze({
+  SHOW_ALL: "all",
+  SHOW_TODO: "todo",
+});
+
+const ERROR_MSG = freeze({
+  NOT_ENOUGH_ARGS: "인자가 충분하지 않습니다",
+  NOT_EXIST_COMMAND: "없는 명령어 입니다.",
+  NOT_EXIST_ID: "없는 id 입니다.",
+  NOT_EXIST_STATUS: "없는 상태입니다.",
+});
+
+module.exports = {
+  STATUS,
+  COMMAND,
+  SUB_COMMAND,
+  ERROR_MSG,
+};
